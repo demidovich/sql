ceac26c1b56ea0f880c67b2939d16ca6ffded936
dm <dmitrij.demidovich@gmail.com>
2020-04-11 20:43:03 +0300

    added jsonb array element functions

---
+++ jsonb_array.sql
@@ -0,0 +1,61 @@
+--
+-- Replace or add value to jsonb array
+--
+CREATE OR REPLACE FUNCTION jsonb_array_set(
+    input_json_array jsonb,
+    current_value text default null,
+    new_value text default null) 
+RETURNS jsonb AS $$
+DECLARE
+    buffer text[];
+BEGIN
+
+    IF jsonb_typeof(input_json_array) != 'array' THEN
+        RETURN input_json_array;
+    END IF;
+
+    IF new_value IS NULL THEN
+        RETURN input_json_array;
+    END IF;
+
+    buffer := ARRAY(SELECT jsonb_array_elements_text(input_json_array));
+
+    IF current_value IS NOT NULL THEN
+        SELECT array_remove(buffer, current_value) INTO buffer;
+    END IF;
+
+    SELECT array_remove(buffer, new_value) INTO buffer;
+    SELECT array_append(buffer, new_value) INTO buffer;
+
+    RETURN array_to_json(buffer);
+
+END;
+    $$ LANGUAGE PLPGSQL;
+
+--
+-- Delete value from jsonb array
+--
+CREATE OR REPLACE FUNCTION jsonb_array_del(
+    input_json_array jsonb,
+    delete_value text default null)
+RETURNS jsonb AS $$
+DECLARE
+    buffer text[];
+BEGIN
+
+    IF jsonb_typeof(input_json_array) != 'array' THEN
+        RETURN input_json_array;
+    END IF;
+
+    IF delete_value IS NULL THEN
+        RETURN input_json_array;
+    END IF;
+
+    buffer := ARRAY(SELECT jsonb_array_elements_text(input_json_array));
+
+    SELECT array_remove(buffer, delete_value) INTO buffer;
+
+    RETURN array_to_json(buffer);
+
+END;
+    $$ LANGUAGE PLPGSQL;
