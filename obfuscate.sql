f160bd07e795908ae092e18fd99538f638c38da2
dm <dmitrij.demidovich@gmail.com>
2020-05-21 13:16:43 +0300

    Replace text generator. Now the same source text has the same generated value. Needed to correct group by

--- obfuscate.sql
+++ obfuscate.sql
@@ -1,15 +1,37 @@
 --
 -- Generate random word
 --
-CREATE OR REPLACE FUNCTION random_word(
+-- CREATE OR REPLACE FUNCTION random_word(
+--     length integer) 
+-- RETURNS text LANGUAGE plpgsql IMMUTABLE AS $$
+-- BEGIN
+
+--     RETURN array_to_string(array(
+--         SELECT SUBSTRING('abcdefghjkmnpqrstuvwxyz' FROM floor(random() * 31)::int + 1 FOR 1)
+--         FROM generate_series(1, $1)
+--     ), '');
+
+-- END;
+-- $$;
+
+--
+-- Generate random word
+--
+CREATE OR REPLACE FUNCTION word_by_hash(
+    hash text,
     length integer) 
 RETURNS text LANGUAGE plpgsql IMMUTABLE AS $$
+DECLARE
+    hash_length integer;
 BEGIN
 
-    RETURN array_to_string(array(
-        SELECT SUBSTRING('abcdefghjkmnpqrstuvwxyz' FROM floor(random() * 31)::int + 1 FOR 1)
-        FROM generate_series(1, $1)
-    ), '');
+    hash_length := length(hash);
+
+    IF length > hash_length THEN
+        hash := REPEAT(hash, CEIL(length / hash_length)::integer);
+    END IF;
+
+    RETURN SUBSTRING(hash from 1 for length);
 
 END;
 $$;
@@ -22,6 +44,7 @@ CREATE OR REPLACE FUNCTION obfuscate_text(
 RETURNS text LANGUAGE plpgsql IMMUTABLE AS $$
 DECLARE
     word text;
+    hash text;
     buffer text[];
 BEGIN
 
@@ -29,8 +52,10 @@ BEGIN
         RETURN input_text;
     END IF;
 
+    hash := md5(input_text);
+
     FOREACH word IN ARRAY regexp_split_to_array(input_text, ' ') LOOP
-        word   := random_word(length(word) + 1);
+        word   := word_by_hash(hash, length(word));
         buffer := array_append(buffer, word);
     END LOOP;
 
